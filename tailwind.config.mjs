1dc663fa8bf4768b1ea1c282d48d693ade60c29a
Ethan Denny <ethandenny@protonmail.com>
2024-06-02 15:43:16 -0230

    Finsih home page layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      white: "#f5f1ea",
+      pink: "#f9a3aa",
+      red: "#f5576c",
+      green: "#004953",
+      blue: "0f282f",
+      black: "070d0d",
+    },
+    fontFamily: {
+      sans: ["Familjen Grotesk", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
