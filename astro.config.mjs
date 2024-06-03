a9b3985762578179f72ccb4092192028e549d7ef
Ethan Denny <ethandenny@protonmail.com>
2024-06-03 19:51:08 -0230

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ethandenny.dev",
   integrations: [tailwind()],
   markdown: {
     rehypePlugins: [
