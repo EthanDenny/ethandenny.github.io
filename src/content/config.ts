7cc56612d8304ac353a4ca50105c7492ee19ca89
Ethan Denny <ethandenny@protonmail.com>
2024-06-22 17:56:20 -0230

    Small style changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,10 @@
 import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: "content", // v2.5.0 and later
+  type: "content",
   schema: z.object({
     title: z.string(),
-    date: z.date(),
+    date: z.string(),
   }),
 });
 
