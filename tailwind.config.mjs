aa41a53b665e8ce06fd0ee0af26d1d0d96cf0ff6
Ethan Denny <ethandenny@protonmail.com>
2024-08-01 19:46:43 -0230

    Add scroll progress

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-  darkMode: "selector",
   theme: {
     colors: {
       white: "#f6f4f1",
@@ -9,7 +8,21 @@ export default {
       orange: "#e84a2e",
     },
     fontFamily: {
-      sans: ["Verdana", "sans-serif"],
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "avenir next",
+        "avenir",
+        "segoe ui",
+        "helvetica neue",
+        "helvetica",
+        "Cantarell",
+        "Ubuntu",
+        "roboto",
+        "noto",
+        "arial",
+        "sans-serif",
+      ],
     },
     extend: {},
   },
